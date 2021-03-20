71848aba4f980f7629dbf3206a565b77171d7627
RocinRykor <briggs.steven369@gmail.com>
2021-03-20 12:25:11 -0700

    added additioonal html pages

--- rrprojects/routes/general.py
+++ rrprojects/routes/general.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, redirect, session, request
+from flask import render_template, Blueprint  # , redirect, session, request
 
 general = Blueprint("general", __name__)
 
@@ -6,3 +6,13 @@ general = Blueprint("general", __name__)
 @general.route("/")
 def index():
     return render_template("index.html")
+
+
+@general.route("/characters/")
+def characters():
+    return render_template("characters.html")
+
+
+@general.route("/decrypt/")
+def decrypt():
+    return render_template("decrypt.html")
