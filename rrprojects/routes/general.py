21f892a42bc7ffdd52cbacf3bbd3ae88aa67785d
Steven <Briggs.steven369@gmail.com>
2022-11-28 23:50:54 -0800

    Added favicon, changed page titles and minor formatting changes

--- rrprojects/routes/general.py
+++ rrprojects/routes/general.py
@@ -5,17 +5,17 @@ general = Blueprint("general", __name__)
 
 @general.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Portfolio Site")
 
 @general.route("/work")
 def work():
-    return render_template("work.html")
+    return render_template("work.html", title="Steven's Projects")
 
 @general.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About Steven")
 
 
 @general.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", title="Contact Steven")
