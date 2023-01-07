96adea2fd115c18709f1d8eea8f2a2985ddaffa6
Steven <Briggs.steven369@gmail.com>
2023-01-07 13:50:21 -0800

    "public" restructure for better organization

--- rrprojects/routes/general.py
+++ rrprojects/routes/general.py
@@ -5,24 +5,24 @@ general = Blueprint("general", __name__)
 
 @general.app_errorhandler(404)
 def custom_error_page(e):
-    return render_template("error.html", title="404 - Page Not Found!")
+    return render_template("public/error.html", title="404 - Page Not Found!")
 
 @general.route("/")
 def index():
-    return render_template("index.html", title="Portfolio Site")
+    return render_template("public/index.html", title="Portfolio Site")
 
 @general.route("/work")
 def work():
-    return render_template("work.html", title="Steven's Projects")
+    return render_template("public/work.html", title="Steven's Projects")
 
 @general.route("/about")
 def about():
-    return render_template("about.html", title="About Steven")
+    return render_template("public/about.html", title="About Steven")
 
 @general.route("/contact")
 def contact():
-    return render_template("contact.html", title="Contact Steven")
+    return render_template("public/contact.html", title="Contact Steven")
 
 @general.route("/testing")
 def testing():
-    return render_template("testing.html", title="JavaScript Testing Grounds")
+    return render_template("public/testing.html", title="JavaScript Testing Grounds")
