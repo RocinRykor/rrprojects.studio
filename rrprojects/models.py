5f1aed978f424c0294facf121f7049958f150fab
Steven <Briggs.steven369@gmail.com>
2023-01-20 19:28:46 -0800

    add colums to projects table

--- rrprojects/models.py
+++ rrprojects/models.py
@@ -52,18 +52,24 @@ class User(db.Model, UserMixin):
 
 class Project(db.Model):
     """
-    | id:            The primary key for the project
-    | title:         A string for the name of the project
-    | repo_link:     URL to the github repository for the project
-    | live_link:     URL to the active page for the project (such as Github Pages) 
-    | description:   Large text desciprion for the project
-    | img_filename:  Filename for an image representing the project 
+    | id:                       The primary key for the project
+    | title:                    A string for the name of the project
+    | repo_link:                URL to the github repository for the project
+    | repo_link_description:    Text to accompany the repo link to allow customization
+    | live_link:                URL to the active page for the project (such as Github Pages)
+    | live_link_description:    Text to accompany the live link to allow customization 
+    | short_description:        Shoty text desciprion for the project, shows on initial project card   
+    | description:              Large text desciprion for the project, shows on full project page
+    | img_filename:             Filename for an image representing the project 
     """
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
     repo_link = db.Column(db.String(256))
+    repo_link_description = db.Column(db.String(256))
     live_link = db.Column(db.String(256))
+    live_link_description = db.Column(db.String(256))
+    short_description = db.Column(db.String(1024))
     description = db.Column(db.String(8192))
     img_filename = db.Column(db.String(128))
 
@@ -78,7 +84,10 @@ class Project(db.Model):
             "id": self.id,
             "title": self.title,
             "repo_link": self.repo_link,
+            "repo_link_description": self.repo_link_description,
             "live_link": self.live_link,
+            "live_link_description": self.live_link_description,
+            "short_description": self.short_description
             "description": self.description,
             "img_filename": self.img_filename,
         }
