b5ae0ceceff849a578ce2783e8c5bfcc7af923bf
Steven <Briggs.steven369@gmail.com>
2023-01-22 07:07:07 -0800

    update app -> application

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ sys.path.append(basedir)
 
 import rrprojects
 
-app = rrprojects.application
+application = rrprojects.application
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    application.run(host="0.0.0.0", debug=True, port=5000)
