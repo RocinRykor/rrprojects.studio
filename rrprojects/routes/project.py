8d9dd36794eaf57706b3150aa6c15ebe094a2cbd
Steven <Briggs.steven369@gmail.com>
2023-04-09 16:16:33 -0700

    Add file checking

--- rrprojects/routes/project.py
+++ rrprojects/routes/project.py
@@ -6,33 +6,42 @@ from flask_login import login_required
 from rrprojects.app import db, Project
 from rrprojects.forms import ProjectForm
 from rrprojects.utils import replace_markdown
+import os
 
 project = Blueprint("project", __name__, url_prefix="/project")
 
 
 @project.route("/")
 def projects():
-    with open('rrprojects/static/json/projects.json') as f:
-        projects_data = json.load(f)
+    try:
+        with open(os.path.join(os.getcwd(), 'rrprojects/static/json/projects.json')) as f:
+            projects_data = json.load(f)
+    except Exception as e:
+        print(f"Error loading projects.json: {e}")
+        projects_data = []
 
     return render_template("public/projects/projects.html",
                            title="Steven's Projects",
                            projects=projects_data,
                            replace_func=replace_markdown)
 
-
 @project.route("/<int:id>")
 def project_details(id):
+    try:
+        with open(os.path.join(os.getcwd(), 'rrprojects/static/json/projects.json')) as f:
+            projects_data = json.load(f)
 
-    with open('rrprojects/static/json/projects.json') as f:
-        projects_data = json.load(f)
+        for project in projects_data:
+            if project['id'] == id:
+                return render_template("public/projects/project_page.html",
+                                       title=project['title'],
+                                       project=project,
+                                       replace_func=replace_markdown)
+    except Exception as e:
+        print(f"Error loading projects.json: {e}")
 
-    for project in projects_data:
-        if project['id'] == id:
-            return render_template("public/projects/project_page.html",
-                                   title=project['title'],
-                                   project=project,
-                                   replace_func=replace_markdown)
+    return "Project not found."
+
 
 @login_required
 @project.route("/add", methods=["GET"])
